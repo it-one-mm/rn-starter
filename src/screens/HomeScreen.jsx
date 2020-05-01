0d08828b81d11f07b1d732920ba9662a07ce268c
IT ONE MM <itone@gmail.com>
2020-05-01 19:17:48 +0630

    first commit

---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+
+const HomeScreen = () => {
+  return <Text style={styles.text}>HomeScreen</Text>;
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
